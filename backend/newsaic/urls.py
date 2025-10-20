2dc86ab295896ee30455d36352dc117948492258
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2025-10-20 21:09:18 +0100

    Enable auth endpoint and add /api endpoint to test auth with DRF

--- backend/newsaic/urls.py
+++ backend/newsaic/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+router = routers.DefaultRouter()
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('api-auth/', include('rest_framework.urls',namespace='rest_framework')),
+    path('api/', include(router.urls)),
 ]
