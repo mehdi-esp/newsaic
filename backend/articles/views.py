4dbcd39a0203a0aeeeb6a560818ce00851201292
Mohamed Amor <99400312+mohamedamor002@users.noreply.github.com>
2025-11-05 20:20:34 +0100

    add article recommendation feature

--- backend/articles/views.py
+++ backend/articles/views.py
@@ -7,6 +7,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework import status
 from users.permissions import BookmarkPermission
+from django_mongodb_backend.expressions import SearchVector
 
 
 class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
@@ -23,6 +24,37 @@ class ArticleViewSet(viewsets.ReadOnlyModelViewSet):
             return Article.objects.filter(section_id__in=section_ids)
 
         return Article.objects.all()
+
+    @action(detail=True, methods=["get"])
+    def recommended_articles(self, request, pk=None):
+        """
+        Return the 3 most similar articles (vector similarity)
+        excluding the article itself.
+        """
+        article = self.get_object()  # 404 if article does not exist
+        if not article.embedding:
+            return Response(
+                {"detail": "This article has no embedding."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        results = (
+            Article.objects.exclude(id=article.id)  # do not recommend itself
+            .annotate(
+                score=SearchVector(
+                    path="embedding",
+                    query_vector=article.embedding,
+                    limit=4,
+                    num_candidates=150,
+                )
+            )
+            .order_by("-score")
+        )
+
+        top_three = results
+
+        serializer = self.get_serializer(top_three, many=True)
+        return Response(serializer.data)
 
     @action(detail=True, methods=['get', 'post', 'delete'], permission_classes=[BookmarkPermission])
     def bookmark(self, request, pk=None):
