74140145307f653fc60f8934a924a859d5873012
mehdi-esp <mohamedmehdi.abid@esprit.tn>
2025-11-08 12:02:44 +0100

    Fix typing

--- backend/utils/embeddings.py
+++ backend/utils/embeddings.py
@@ -3,5 +3,5 @@ import ollama
 
 EMBEDDING_MODEL = "qwen3-embedding:0.6B"
 
-def embed(inputs: list[str]) -> list[float]:
+def embed(inputs: list[str]) -> list[list[float]]:
     return ollama.embed(EMBEDDING_MODEL, inputs)["embeddings"]
